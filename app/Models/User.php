cd9e39c8e9c6181b93a50d3f2902cd69c55ea55f
jbmll <juniorbmorales@gmail.com>
2025-07-27 12:05:56 -0500

    feat: Modelo User

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use OwenIt\Auditing\Contracts\Audit;
+use OwenIt\Auditing\Auditable;
+use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
 
-class User extends Authenticatable
+class User extends Authenticatable implements AuditableContract 
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, Auditable;
 
     /**
      * The attributes that are mass assignable.
@@ -18,9 +21,11 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'socio_id',
         'email',
         'password',
+        'rol',
+        'ultimo_login',
     ];
 
     /**
@@ -32,6 +37,11 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public function socio()
+    {
+        return $this->belongsTo(Socio::class);
+    }
 
     /**
      * Get the attributes that should be cast.
