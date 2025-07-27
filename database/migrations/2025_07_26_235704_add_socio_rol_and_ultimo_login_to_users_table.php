f9b5da980b9b91d83b854dc5a362a10659825d08
Ooops <dev.seb.3745@gmail.com>
2025-07-26 21:49:12 -0500

    update nullable

--- database/migrations/2025_07_26_235704_add_socio_rol_and_ultimo_login_to_users_table.php
+++ database/migrations/2025_07_26_235704_add_socio_rol_and_ultimo_login_to_users_table.php
@@ -4,25 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            
+
             $table
                 ->foreignId('socio_id')
                 ->after('id')
+                ->nullable()
                 ->constrained('socios')
                 ->onDelete('cascade');
 
-            
+
             $table
                 ->enum('rol', ['socio', 'admin'])
                 ->after('password')
                 ->default('socio');
 
-         
+
             $table
                 ->timestamp('ultimo_login')
                 ->after('rol')
